e55fd9c590b6b3bcbacc7e3de132de7952ee39ff
José Félix Longares <josefelix_96@hotmail.com>
2019-02-28 21:37:52 +0100

    Merge branch 'master' of /home/pepelongares/Desktop/hashCode/hashcode1819 with conflicts.

--- src/main/kotlin/Slide.kt
+++ src/main/kotlin/Slide.kt
@@ -1,36 +1,17 @@
 class Slide (val photo1: Photo, val photo2: Photo? = null) {
-    init {
-        assert(!(photo1.orientation == Photo.Orientation.VERTICAL && photo2 == null))
-        assert(photo1.orientation == Photo.Orientation.HORIZONTAL && photo2 != null)
-    }
 
 
     val topics: MutableList<Topic>
-        get() {
-            var topics: MutableList<Topic> = ArrayList()
-            var resultado: MutableList<Topic> = ArrayList()
-            var aux: MutableList<String> = ArrayList()
 
-            topics = photo1.topics
 
-            if (photo2 != null) {
-                for (topic in photo2.topics) {
-                    topics.add(topic)
-                }
-            }
+    init {
+        assert(!(photo1.orientation == Photo.Orientation.VERTICAL && photo2 == null))
+        assert(photo1.orientation == Photo.Orientation.HORIZONTAL && photo2 != null)
+        this.topics = ArrayList()
 
-            for (topic in topics) {
-                aux.add(topic.topic)
-            }
-
-            aux = aux.distinct().toMutableList()
-            /*val set: Set<Topic> = HashSet<Topic>(topics)
-            topics.clear()
-            topics.addAll(set)*/
-            for (topicName in aux){
-                resultado.add(Topic(topicName))
-            }
-            //return topics.distinct().toMutableList()
-            return resultado
+        this.topics.addAll(photo1.topics)
+        if (photo2 != null) {
+            this.topics.addAll(photo2.topics)
+        }
     }
 }
